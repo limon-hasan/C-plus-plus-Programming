1885c0f4b6a8a46cdd092b60f5994beb7c5cac92
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-05-30 01:32:23 +0600

    Create datatype.cpp

---
+++ C++ Codes/datatype.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    num1 = 10.34;
+    num2 = 30.45980;
+    char ch;
+    ch = 'C';
+
+    cout << "Character = " << ch << endl << "Number 1 = " << num1 <<endl << "Number 2 = " << num2 <<endl;
+
+    getch();
+}
