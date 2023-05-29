734e3e4164c3bef85e6d64a17f5439139f7422bb
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-05-29 12:34:36 +0600

    Add files via upload

---
+++ C++ Codes/biodata.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    cout << "This is a new pen\n";
+    cout << "This " << "is " << "a " << "new " << "pen\n";
+    cout << "HELLO!" << " This is a game show" << " And here it is" ;
+
+    getch();
+
+}
+
