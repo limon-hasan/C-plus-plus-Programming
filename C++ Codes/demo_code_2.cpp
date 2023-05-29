3d4416fb91893c8929563483608586be11e0b171
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-05-29 12:58:07 +0600

    Create demo_code_2.cpp

---
+++ C++ Codes/demo_code_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    cout << "This is a new pen" << endl;   // << endl hocche ekta command. \n na diye << endl use korle line new line e chole jabe
+    cout << "This " << "is " << "a " << "new " << "pen" <<endl;
+    cout << "\"HELLO!" << " This is a game show" << " And here it is\"" << endl;
+    
+    cout << "This is Bangladesh" <<" I live in DHAKA" << endl;
+    cout << "This is Bangladesh" << endl << "I live in DHAKA" << endl;
+    cout << "Hi I am LIMON" << endl;
+    
+    getch();
+}
