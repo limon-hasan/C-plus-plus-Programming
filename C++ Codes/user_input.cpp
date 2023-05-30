75daca39a7e173de1bec7713edec356d15248d57
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-05-31 02:16:06 +0600

    Update user_input.cpp

--- C++ Codes/user_input.cpp
+++ C++ Codes/user_input.cpp
@@ -16,7 +16,7 @@ int main()
                                  Later, when you try to use cin.getline(name, 30); to read a line of input for the name, the newline character left in the input buffer from the previous cin >> num; is consumed immediately. As a result, getline() reads an empty line or an empty string for the name.
                                  To handle this issue, you can use cin.ignore(); right after reading the integer to clear the newline character from the input buffer. The cin.ignore() function discards the newline character and any other characters left in the input buffer, allowing you to proceed with the next input operation without any issues.
                                  */
-    cin.getline(name,30);
+    cin.getline(name,30);        // cin>> name na niye eita newa hoyeche karon cin>> name shudhu ekta word input ney, space er pore input nite pare na ejonno cin.getline(name,30) nisi
     cout << "Name: " << name;
 
     getch();
