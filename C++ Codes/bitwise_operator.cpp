05b3224bd3b41c7f1b33479583cc1b5e7dd6073e
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-06-02 22:49:23 +0600

    Create bitwise_operator.cpp

---
+++ C++ Codes/bitwise_operator.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x =12, y=32;
+
+    int r = x&y;
+    cout << r<< endl;
+
+    r = x|y;
+    cout << r<<endl;
+
+    r = x^y;
+    cout << r<<endl;
+    
+    r = y>>3;
+    cout << r<<endl;
+
+    r = y<<3;
+    cout << r<<endl;
+
+}
