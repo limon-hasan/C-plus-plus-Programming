5404a1a442189806094b5349f5c13ab7fdb314fb
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-10-22 16:11:58 +0600

    Create string- remove letter.cpp

---
+++ C++ Codes/string- remove letter.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+string removeLetter(string &input, char letter);
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    char remove;
+    cin >> remove;
+
+    string modified = removeLetter(str, remove);
+    cout << "Original string = " << str << endl;
+    cout << "Modified string = " << modified << endl;
+    return 0;
+}
+
+
+string removeLetter(string &input, char letter)
+{
+    string result;   // input string
+    for(char a : input)
+    {
+        if(a!= letter)
+            result = result+a;
+    }
+    return result;
+}
+
+
+
+Or------------------------
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+string removeLetter(string &input, char letter);
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    char remove;
+    cin >> remove;
+
+    string modified = removeLetter(str, remove);
+    cout << "Original string = " << str << endl;
+    cout << "Modified string = " << modified << endl;
+    return 0;
+}
+
+
+string removeLetter(string &input, char letter)
+{
+    string result;   // input string
+    for(char a : input)
+    {
+        if(a!= letter)
+            result = result+a;
+    }
+    return result;
+}
+  
