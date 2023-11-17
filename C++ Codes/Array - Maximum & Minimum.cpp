55d58d4dd9a5c46e0736da71e989298ca1a3de39
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-17 19:21:56 +0600

    Update Array - Maximum & Minimum.cpp

--- C++ Codes/Array - Maximum & Minimum.cpp
+++ C++ Codes/Array - Maximum & Minimum.cpp
@@ -1,3 +1,5 @@
+// Without using built-in function
+
 #include <bits/stdc++.h>  
 using namespace std;
 
@@ -22,6 +24,54 @@ int getMax(int arr[], int n) {
 	}
     // Returning max
 	return max;
+}
+
+int main() {
+	int size;
+	cin >> size;
+	int arr[100];
+	for(int i=0; i<size; i++) {
+		cin >> arr[i];
+	}
+	getMax(arr, size);
+	getMin(arr, size);
+
+	cout << "Maximum is " << getMax(arr, size) << endl;
+	cout << "Minimun is " << getMin(arr, size) << endl;
+	
+}
+
+----------------------------------------------------
+	
+// Using built-in function
+
+
+/#include <bits/stdc++.h>  
+using namespace std;
+
+// minimum value
+int getMin(int arr[], int n) {
+	int mini = INT_MAX;
+	for(int i=0; i<n; i++) {
+		mini = min(mini, arr[i]);   // using built-in function
+		// if(arr[i] < min)
+		// min = arr[i];
+	}
+
+    // Returning min
+	return mini;
+}
+
+// maximum value
+int getMax(int arr[], int n) {
+	int maxi = INT_MIN;
+	for(int i=0; i<n; i++) {
+		 maxi = max(maxi, arr[i]);   // using built-in function
+		// if(arr[i] > max)
+		// max = arr[i];
+	}
+    // Returning max
+	return maxi;
 }
 
 int main() {
