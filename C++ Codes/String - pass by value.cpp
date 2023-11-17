aa41980df67757e27dcf1d3f93bead706800110d
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-17 14:20:17 +0600

    Create String - pass by value.cpp

---
+++ C++ Codes/String - pass by value.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>  // String
+using namespace std;
+
+void printName(string nam) {
+	nam[0] = 'l';
+	cout << nam << endl;
+}
+
+int main() {
+	string name;
+	cin >> name;
+	printName(name);
+	cout <<name;
+}
