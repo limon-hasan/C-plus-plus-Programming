3a6a2b5a4b676759f8cd9efd4e54d8228622c99b
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-18 12:18:00 +0600

    Update Array - swap Alternate.cpp

--- C++ Codes/Array - swap Alternate.cpp
+++ C++ Codes/Array - swap Alternate.cpp
@@ -25,9 +25,9 @@ int main() {
 }
 
 
---------------------------------------------------------------------
 
 // Or ----->  
+
 
 #include <bits/stdc++.h>  
 using namespace std;
@@ -57,3 +57,40 @@ int main() {
 	printAlternate(odd, 5);
 	return 0;
 }  
+
+
+----------------------------------------------------------------------------
+	
+// Without using swap function
+
+#include <bits/stdc++.h>  
+using namespace std;
+void printAlternate(int arr[], int n) {
+	for(int i=0; i<n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void swapAlternate(int arr[], int n)
+{
+	for(int i=0; i<n; i+=2) {
+		int temp;
+		if((i+1) < n) {
+			temp = arr[i];
+			arr[i] = arr[i+1];
+			arr[i+1] = temp;
+		}		
+	}
+}
+
+int main() {
+    int even[8] = {5,2,9,4,7,6,1,0};  // After swapping alternate --->  2,5,4,9,6,7,0,1
+	int odd[5] = {11,33,9,76,43};     // After swapping alternate ---> 33,11,76,9,43
+
+	swapAlternate(even, 8);
+	printAlternate(even, 8);
+	swapAlternate(odd, 5);
+	printAlternate(odd, 5);
+	return 0;
+}
