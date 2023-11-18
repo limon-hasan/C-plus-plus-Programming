e133163942c428b214873e5acb5553e1f4123c19
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-18 12:01:23 +0600

    Create Array - swap Alternate.cpp

---
+++ C++ Codes/Array - swap Alternate.cpp
@@ -0,0 +1,58 @@
+//  Swap Alternate element
+
+#include <bits/stdc++.h>  
+using namespace std;
+void swapAlternate(int arr[], int n)
+{
+	for(int i=0; i<n; i+=2) {
+		if((i+1) < n)
+		swap(arr[i], arr[i+1]);
+	}
+	for(int i=0; i<n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+    int even[8] = {5,2,9,4,7,6,1,0};  // After swapping alternate --->  2,5,4,9,6,7,0,1
+	int odd[5] = {11,33,9,76,43};     // After swapping alternate ---> 33,11,76,9,43
+
+	swapAlternate(even, 8);
+	swapAlternate(odd, 5);
+  return 0;
+}
+
+
+--------------------------------------------------------------------
+
+// Or ----->  
+
+#include <bits/stdc++.h>  
+using namespace std;
+
+void swapAlternate(int arr[], int n)
+{
+	for(int i=0; i<n; i+=2) {
+		if((i+1) < n)
+		swap(arr[i], arr[i+1]);
+	}
+}
+
+void printAlternate(int arr[], int n) {
+	for(int i=0; i<n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+    int even[8] = {5,2,9,4,7,6,1,0};  // After swapping alternate --->  2,5,4,9,6,7,0,1
+	  int odd[5] = {11,33,9,76,43};     // After swapping alternate ---> 33,11,76,9,43
+
+	swapAlternate(even, 8);
+	printAlternate(even, 8);
+	swapAlternate(odd, 5);
+	printAlternate(odd, 5);
+	return 0;
+}  
