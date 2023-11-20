ee0998427e2bfae6a4887ec0d96ea7c3466cb379
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-20 12:03:33 +0600

    Create Vector.cpp

---
+++ C++ Codes/Vector.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;    // size how many elements I want to take
+    vector<int > arra; // vector
+    for(int i=0; i<n; i++) {
+     int element;
+     cin >> element;   // taking input of element
+     arra.push_back(element);  // storing them into vector arra
+    }
+
+    for(int a : arra) {
+     cout << a << " " ;
+    }
+
+    return 0;
+}
+
+
+
+// Again --->
+//  Without taking input of vector  --->
+
+/*
+    vector < int > myVector = {2,5,7,9,1};
+    for(int i: myVector) {
+      cout << i << " ";
+    }
+}
+
+*/
