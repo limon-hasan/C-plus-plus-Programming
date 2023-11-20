95017c570cffcca259952d391782e45182ad7c47
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-20 15:57:05 +0600

    Update Vector - taking input using function.cpp

--- C++ Codes/Vector - taking input using function.cpp
+++ C++ Codes/Vector - taking input using function.cpp
@@ -25,3 +25,37 @@ int main() {
 }
 
 
+-------------------------------------------------------------
+
+// Returning vector from a function 
+
+    
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> inputVector(vector<int> arrayVector, int size)   // 'inputVector' holo function and eta jehetu ekta vector rturn korbe tai eitar return type vector hobe 'vector<int>'
+{
+    for(int i=0; i<size; i++) {
+      int element;
+      cin >> element;   // storing them into vector myArray
+      arrayVector.push_back(element);    // storing them into vector myArray
+    }
+    return arrayVector;
+}
+
+int main() {
+    vector<int> myVector;    // Initialize the vector in the main function
+    int n;
+    cin >> n;    // size how many elements I want to take
+    vector<int>outVector = inputVector(myVector, n);   // Call the function to populate the vector and receive the modified vector by another vector 'outVector'
+                                                      //  Ei line ta emneo likha jay ---> 
+                                                      /*  vector<int> outVector;
+                                                          outVector = inputvector(myVector, n);    */       
+    
+    //printing the element of vector 'myVector' from 'inputVector' function
+    for(int a : outVector) {
+     cout << a << " ";
+    }
+    return 0;
+}
