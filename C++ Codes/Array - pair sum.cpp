d1875db5af239cf8b4471a1576fa32713948d743
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-22 04:36:27 +0600

    Create Array - pair sum.cpp

---
+++ C++ Codes/Array - pair sum.cpp
@@ -0,0 +1,21 @@
+// Given an array and size n and find the pair of that array such that sum all pair equals to n 
+//  https://bit.ly/3EwlU6e
+
+
+#include <bits/stdc++.h>
+// vector<vector<int>> matrix
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   vector<vector<int>> ans;
+   for(int i=0; i<arr.size(); i++) {
+      for(int j=i+1; j<arr.size(); j++) {
+        if (arr[i] + arr[j] == s) {
+          vector<int> temp;
+          temp.push_back(min(arr[i], arr[j]));
+          temp.push_back(max(arr[i], arr[j]));
+          ans.push_back(temp);
+        }
+      }
+   }
+   sort(ans.begin(), ans.end());
+   return ans;
+}
