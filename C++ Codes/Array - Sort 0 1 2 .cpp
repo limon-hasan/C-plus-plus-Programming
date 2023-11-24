59060eec6d12129afb9960faac0b24c74b78638e
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-25 02:55:27 +0600

    Update Array - Sort 0 1 2 .cpp

--- C++ Codes/Array - Sort 0 1 2 .cpp
+++ C++ Codes/Array - Sort 0 1 2 .cpp
@@ -12,7 +12,10 @@ Explanation:
 0s 1s and 2s are segregated into ascending order.
 ***********/
 
-// ----------------------------------------------------------------------
+
+// -------------------------------------------------------------------
+
+// Better Aproach  ------>
 
 #include <bits/stdc++.h> 
 void sort012(int *arr, int n)
@@ -40,3 +43,30 @@ void sort012(int *arr, int n)
         arr[k] = 2;
     }
 }
+
+
+
+// OR -------------------------------------------------------------
+
+// Optimal Approach (Dutch National Flag Algorithm)
+
+#include <bits/stdc++.h> 
+void sortArray(vector<int>& arr, int n) 
+{
+    int low = 0, mid= 0, high = n-1;
+    while(mid <= high) {
+        if(arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
