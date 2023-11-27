5b45c32f23fa5127bb9d9b67352c1e9219a498fd
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-28 00:10:14 +0600

    Create Pattern - 1.cpp

---
+++ C++ Codes/Pattern - 1.cpp
@@ -0,0 +1,21 @@
+// Pattern - 1
+//  https://bit.ly/3QfK2k3
+// Coding-Ninja Problem
+
+/*  
+
+* * *
+* * *
+* * *
+
+*/
+
+void nForest(int n) {
+
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
