3548b50f016ef15661aaf721ee2edf31e67dee75
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-28 06:56:20 +0600

    Create Pattern - 4.cpp

---
+++ C++ Codes/Pattern - 4.cpp
@@ -0,0 +1,21 @@
+// Pattern - 4
+// https://bit.ly/3VADLAt
+// Coding-Ninja Problem
+
+/*  
+
+1 
+2 2 
+3 3 3
+4 4 4 4
+
+*/
+
+void triangle(int n) {
+for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout << i <<" ";
+        }
+        cout << endl;
+    }
+}
