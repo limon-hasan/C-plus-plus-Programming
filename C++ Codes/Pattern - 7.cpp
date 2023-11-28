f8d5411e4ed2cb94cd08dbc22f2a15780187660f
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-29 01:04:08 +0600

    Create Pattern - 7.cpp

---
+++ C++ Codes/Pattern - 7.cpp
@@ -0,0 +1,41 @@
+// Pattern - 7
+// https://bit.ly/3VADLAt
+// Coding-Ninja Problem
+
+/*  
+
+1 2 3 4
+1 2 3
+1 2
+1
+
+*/
+
+
+void nStarTriangle(int n) {
+for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n-i; j++) {
+            cout << " ";
+        }
+        for(int j=1; j<=2*i-1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+
+Or   ------------>
+
+  
+void nStarTriangle(int n) {
+for(int i=0; i<n; i++) {
+        for(int j=1; j<n-i; j++) {
+            cout << " ";
+        }
+        for(int j=1; j<=2*i+1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
