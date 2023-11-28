37ba17fd94abe5db49021b244accba016f603ea1
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-29 01:11:07 +0600

    Update Pattern - 7.cpp

--- C++ Codes/Pattern - 7.cpp
+++ C++ Codes/Pattern - 7.cpp
@@ -39,3 +39,25 @@ for(int i=0; i<n; i++) {
         cout << endl;
     }
 }
+
+
+Or   ------>
+
+void nStarTriangle(int n) {
+for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n-i; j++) {
+            cout << " ";
+        }
+        for(int j=1; j<=2*i-1; j++) {
+            cout << "*";
+        }
+        for(int j=1; j<=n-i; j++) {
+            cout << " ";
+        } 
+        cout << endl;
+    }
+}        
+
+
+
+
