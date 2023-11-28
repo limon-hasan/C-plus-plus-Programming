a3e138a2fe473c622857fd732b3b2a5b455ff5be
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-29 00:18:30 +0600

    Create Pattern - 5.cpp

---
+++ C++ Codes/Pattern - 5.cpp
@@ -0,0 +1,33 @@
+// Pattern - 5
+// https://bit.ly/3VADLAt
+// Coding-Ninja Problem
+
+/*  
+
+* * * *
+* * *
+* * 
+*
+
+*/
+
+void seeding(int n) {
+for(int i=n; i>=1; i--) {
+        for(int j=1; j<=i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+
+Or ----->
+
+void seeding(int n) {
+for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n-i+1; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}  
