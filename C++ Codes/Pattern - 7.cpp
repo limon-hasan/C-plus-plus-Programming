27f50a2eaff8df6669e09bf69cd54716477201ed
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-29 01:27:05 +0600

    Update Pattern - 7.cpp

--- C++ Codes/Pattern - 7.cpp
+++ C++ Codes/Pattern - 7.cpp
@@ -43,6 +43,8 @@ for(int i=0; i<n; i++) {
 
 Or   ------>   // printing space, then star, then space
 
+(1)-------->
+
 void nStarTriangle(int n) {
 for(int i=1; i<=n; i++) {
         for(int j=1; j<=n-i; j++) {
@@ -54,6 +56,24 @@ for(int i=1; i<=n; i++) {
 
         // last e space print na korleo hobe karon new line e gele space er proyojon nai, tobe dileo hoy
         for(int j=1; j<=n-i; j++) {   
+            cout << " ";
+        } 
+        cout << endl;
+    }
+}        
+
+
+(2)-------->
+
+void nStarTriangle(int n) {
+for(int i=0; i<n; i++) {
+        for(int j=1; j<=n-i-1; j++) {
+            cout << " ";
+        }
+        for(int j=1; j<=2*i+1; j++) {
+            cout << "*";
+        }
+        for(int j=1; j<n-i-1; j++) {
             cout << " ";
         } 
         cout << endl;
