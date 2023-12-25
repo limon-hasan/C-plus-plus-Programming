4f25c3e4c5d5ae06d767577c2866374bd513a484
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-12-25 10:54:00 +0600

    Update String - Count words in string.cpp

--- C++ Codes/String - Count words in string.cpp
+++ C++ Codes/String - Count words in string.cpp
@@ -1,6 +1,17 @@
 // C++ program to count words in  
 // a string using stringstream.
 
+/***********
+A stringstream associates a string object with a stream allowing you to read from the string as if it were a stream (like cin). To use stringstream, we need to include sstream header file. The stringstream class is extremely useful in parsing input. 
+
+Basic methods are:
+`(1)` clear()- To clear the stream.
+`(2)` str()- To get and set string object whose content is present in the stream. 
+`(3)` operator <<- Add a string to the stringstream object. 
+`(4)` operator >>- Read something from the stringstream object.
+
+******************/
+
 
 #include<iostream>
 #include <sstream>
