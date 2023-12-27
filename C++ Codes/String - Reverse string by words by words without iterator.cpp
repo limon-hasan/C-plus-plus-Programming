339b6d2b0f076fcc52c7f38c827e54a4ce629cf3
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-12-28 01:03:06 +0600

    Create String - Reverse string by words by words without iterator.cpp

---
+++ C++ Codes/String - Reverse string by words by words without iterator.cpp
@@ -0,0 +1,36 @@
+// without using iterator and using iterator loop
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// without using iterator
+string reverseString(string& s)
+{
+    string reversed;
+    for(auto it = s.rbegin(); it != s.rend(); it++)
+    {
+        reversed.push_back(*it);
+    }
+    return reversed;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    cin.ignore();
+    for(int i=0; i<T; i++)
+    {
+        string str;
+        getline(cin, str);
+        string word;
+        stringstream ss(str);  /*  If the string is "I am a good boy" then, The stringstream breaks the input string into individual words: "I", "am", "a", "good", "boy".  */
+        while(ss >> word)
+        {
+            cout << reverseString(word) <<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
