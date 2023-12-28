1325bd5ed84e9cf5dba49dca342a6eb4d2c22dd5
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-12-28 19:02:45 +0600

    Update Pairs - Utility library.cpp

--- STL/Pairs - Utility library.cpp
+++ STL/Pairs - Utility library.cpp
@@ -7,7 +7,7 @@ void explainPair() {
 
 	// Initialize two variables in a paired way
 	pair<int, int> p = {1,3};  // pairs is a part of utility library
-  cout << p.first;  // printing first elements of pair
+        cout << p.first;  // printing first elements of pair
 	cout << endl;
 	cout << p.first << " " << p.second; // printing elements of pair
 	cout << endl;
@@ -17,12 +17,12 @@ void explainPair() {
 	cout << f.first << endl;
 	cout << f.second.first << endl;
 	cout << f.first << " " << f.second.second <<" "<< f.second.first;
-  cout << endl;
+        cout << endl;
 
 	// Storing pairs in array indexes. Pair can be treated as a data type
 	pair<int, int> arr[] = {{5,8}, {2,7}, {9,0}, {4,6}, {8,1}}; // Pair Array
 	cout << arr[2].first << endl;
-  cout << arr[1].first << endl;
+        cout << arr[1].first << endl;
 	cout << arr[4].second << endl;
 	cout<< arr[3].first << endl;
 }
