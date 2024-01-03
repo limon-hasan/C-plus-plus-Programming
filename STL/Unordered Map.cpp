69270b4df72e92aac141e0f3e44c9dda9c0ec604
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-01-04 02:02:04 +0600

    Create Unordered Map.cpp

---
+++ STL/Unordered Map.cpp
@@ -0,0 +1,21 @@
+// C++ STL
+
+// Multimap
+// Unordered Map in all cases works in constant time --> O(1)
+// In worst cases, it works for O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainUnorderedMap() {
+
+	// Multimap is similiar to map, only it will not store in sorted. It will be randomized
+    // It will have unique keys, but it will not be sorted
+
+}
+
+int main() {
+	
+	explainUnorderedMap();
+	return 0;
+}
