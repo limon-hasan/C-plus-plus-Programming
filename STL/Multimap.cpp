959776c8bdd64257bf1a561a8d44b9f1525ab506
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-01-04 01:54:27 +0600

    Create Multimap.cpp

---
+++ STL/Multimap.cpp
@@ -0,0 +1,19 @@
+// C++ STL
+
+// Multimap
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainMultimap() {
+
+	  // Multimap is similiar to map, only it can store duplicate keys 
+    // It is also sorted
+
+}
+
+int main() {
+	
+	explainMultimap();
+	return 0;
+}
