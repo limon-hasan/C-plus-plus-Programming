a2651ff4c2f0a2ecba16e0aae1ffdbe363957033
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-01-20 04:11:04 +0600

    Update Recursion - Print name N times.cpp

--- C++ Codes/Recursion - Print name N times.cpp
+++ C++ Codes/Recursion - Print name N times.cpp
@@ -1,3 +1,6 @@
+// Problem link : https://www.codingninjas.com/studio/problems/-print-n-times_8380707?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+// Two solution -->
+
 // Print name N times using recursion
 
 // Time complexity -> O(n) 
@@ -5,6 +8,9 @@
    Here, space complexity is hypothetical because the computer's internal memory uses the stack space
 */
 
+
+// (i) -->
+// Normal Solution without using vector : -->
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -23,4 +29,73 @@ int main() {
     print(1, n);  // n = 4
 
     return 0;
+}
+
+
+------------------------------------------
+
+// (ii) -->
+// Solution 1 -->
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void recursiveName(int i, int x, vector<string> &name) {
+	if(i > x)
+		return;
+	else{
+		name.push_back("Coding Ninjas");
+		recursiveName(i+1, x, name);
+	}
+}
+vector<string> printNTimes(int n) {
+	vector<string> ans;
+	recursiveName(1, n, ans);
+	return ans;
+}
+
+int main() {
+	int n;
+	cin >>n;
+	vector<string> result;
+	result = printNTimes(n);
+
+	for(const string a : result) {
+		cout <<a <<" ";
+	} cout << endl;
+}
+
+
+-------------------------------------------------
+
+// (iii) -->
+// Solution 2 : -->
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void recursiveName(int x, vector<string> &name) {
+	if(x == 0)
+		return;
+	else{
+		name.push_back("Coding Ninjas");
+		recursiveName(x-1, name);
+	}
+}
+vector<string> printNTimes(int n) {
+	vector<string> ans;
+	recursiveName(n, ans);
+	return ans;
+}
+
+int main() {
+	int n;
+	cin >>n;
+	vector<string> result;
+	result = printNTimes(n);
+
+	for(const string a : result) {
+		cout <<a <<" ";
+	} cout << endl;
 }
