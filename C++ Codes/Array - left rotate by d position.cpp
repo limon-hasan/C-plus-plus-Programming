dfccaecca9adff9186b01be172a120a753d231a3
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-01-25 19:48:25 +0600

    Update Array - left rotate by d position.cpp

--- C++ Codes/Array - left rotate by d position.cpp
+++ C++ Codes/Array - left rotate by d position.cpp
@@ -4,53 +4,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> rotateArray(vector<int> &arr, int k) {
+vector<int> rotateArray(vector<int> &arr, int k)
+{
 
     int n = arr.size();
     // number of positions to rotate
-    k = k % n;    
+    k = k % n;
 
-	vector<int> temp;
-	// storing into temp vector
-	for(auto i = 0; i < k; i++) {
-		temp.push_back(arr[i]);
-	}
+    vector<int> temp;
+    // storing into temp vector
+    for(auto i = 0; i < k; i++)
+    {
+        temp.push_back(arr[i]);
+    }
 
-	// shifting to arr vector
-	for(int i = k; i < n; i++) {
-		arr[i - k] = arr[i];
-	}
+    // shifting to arr vector
+    for(int i = k; i < n; i++)
+    {
+        arr[i - k] = arr[i];
+    }
 
-	// put them from temp into arr vector [using mathematical calculation]
-    for(int i = n - k; i < n; i++) { 
-    	arr[i] = temp[i - (n - k)];
+    // put them from temp into arr vector [using mathematical calculation]
+    for(int i = n - k; i < n; i++)
+    {
+        arr[i] = temp[i - (n - k)];
     }
 
     /*  uporer last loop ta evabeo lekha jay -> put them from temp into arr vector [without mathematical calculation]
     int j = 0;
     for(int i = n - k; i<n; i++) {
         arr[i] = temp[j];
-        j++;	
+        j++;
     }
     */
     return arr;
 }
 
-int main() {
-	int n;
-	cin >> n;
+int main()
+{
+    int n;
+    cin >> n;
 
-	vector<int> arr;    // 1, 2, 3, 4, 5, 6, 7
-	for(int i = 0; i<n; i++) {
-		int value;
-		cin >> value;
-		arr.push_back(value);
-	}
-	int d;
-	cin >> d;
-	rotateArray(arr, d);
+    vector<int> arr;    // 1, 2, 3, 4, 5, 6, 7
+    for(int i = 0; i<n; i++)
+    {
+        int value;
+        cin >> value;
+        arr.push_back(value);
+    }
+    int d;
+    cin >> d;
+    rotateArray(arr, d);
 
-	for(auto a : arr) {
-		cout << a << " ";    // 4, 5, 6, 7, 1, 2, 3
-	}  cout << endl;
+    for(auto a : arr)
+    {
+        cout << a << " ";    // 4, 5, 6, 7, 1, 2, 3
+    }
+    cout << endl;
 }
