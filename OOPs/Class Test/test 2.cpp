88604c6b1c24187f5b5b35b825d29b16c922b92b
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-02-19 00:49:08 +0600

    Update test 2.cpp

--- OOPs/Class Test/test 2.cpp
+++ OOPs/Class Test/test 2.cpp
@@ -1,1 +1,96 @@
+// Find the output of the following -->
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student { 
+    private:
+    string name;
+    double cgpa;
+    string phone;
+
+    public:
+    string Department;
+    Student() {
+        name = "Daniel";
+        cgpa = 0.00;
+        phone = "017000000000";
+        Department = "CSE";
+    }
+
+    void printCgpa() {
+        cout << name <<" " <<  cgpa << endl;
+    }
+
+    void setName(string n) {
+        name = n;
+    }
+
+    void setCgpa(double c) {
+        cgpa = c;
+    }
+
+    void setDepartment(string d) {
+        Department = d;
+    }
+
+    string getDepartment() {
+        return name;
+    }
+
+    double getName() {
+        return cgpa;
+    }
+
+    double getPhone() {
+        return cgpa;
+    }
+
+    void printDetails() {
+        cout << name <<" "<< cgpa<<" " << phone<<" " << Department <<" "<< endl;
+    }
+
+    ~Student() {
+        cout << "Done" << endl;
+    }
+};
+
+int main() {
+    Student Rafi;
+    Student Limon;
+    Student Tamim;
+    Rafi.setName("Tamim");
+    Limon.setCgpa(3.70);
+    Tamim.setDepartment("CS");
+    // Limon.setPhone("019444");
+    Rafi.setCgpa(3.50);
+    Tamim.printCgpa();
+    Rafi.printDetails();
+    Tamim.setCgpa(Rafi.getName());
+    Rafi.setName(Limon.getDepartment());
+    Limon.Department = "CS";
+    Rafi.printDetails();
+    Limon.printDetails();
+    Rafi.printDetails();
+    Tamim.printCgpa();
+
+    cout << Rafi.getName()<<" " << Tamim.getPhone() <<" "<< endl;
+    cout << "good luck"<< endl;
+
+    return 0;
+}
+
+/*
+Output : ->
+Daniel 0
+Tamim 3.5 017000000000 CSE 
+Daniel 3.5 017000000000 CSE 
+Daniel 3.7 017000000000 CS 
+Daniel 3.5 017000000000 CSE 
+Daniel 3.5
+3.5 3.5 
+goof luck
+Done
+Done
+Done
+*/  
