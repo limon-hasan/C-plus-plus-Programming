df04c2bfe8d2ac24b130dae2f43089d380b66d70
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-03-06 12:01:49 +0600

    Create basic.cpp

---
+++ OOPs/Encapsulation/basic.cpp
@@ -0,0 +1,11 @@
+// Encapsulation --> wrapping up data member and fucntions
+// Encapsulation --> information hiding/data hiding
+// Fully Encapsulated class --> all data memebesr --> private  // that means we can access this members in same class 
+// we cannot access this members from different class 
+
+/* Advantage of Encapsulation -->
+1. Data hide  --> security increasing
+2. If we want, we can make class "Read Only"
+3. Code Resuabiluty
+4. Encapsulated code is better for unit testing 
+*/
