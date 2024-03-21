48b40952c230c531813a51937504d82877e3bb6b
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-03-21 20:37:24 +0600

    Create private mode - private_Super class & public_Child class.cpp

---
+++ OOPs/Inheritance/private mode - private_Super class & public_Child class.cpp
@@ -0,0 +1,59 @@
+// Inheritance - memebr of the Super class is in private mode
+
+// When the member of the super class is in private mode and the child class inherits it from publicly, private member will remain private within the child(Male) class and they will be accessible wihtin the super class and the child class but not be accessible outside the super and child class
+
+#include <iostream>
+using namespace std;
+
+class Human {
+
+private:
+	  int age;
+protected:   
+	  int height;
+public:
+	  int weight;
+
+public:
+	  Human() {
+		cout <<"HI" << endl;
+		cout << "Weight is: "<< this->weight << endl;
+	}
+
+	  int getAge() {
+		return this->age;
+	}
+};
+
+class Male : public Human {
+
+public:
+	  string color;
+
+	  Male() {
+		cout <<"HELLO" << endl;
+	  }
+
+	  void sleep() {
+		cout <<"Male sleeping " << endl;
+	  }
+
+    // This following part shows error :-->
+	  void print(int p) {
+		age = p;
+		cout << "Now, age is: " << age << endl;  // this will show compilation error --> because 'age' is a private member in the parent(Human) class that's why it cannot be accessible directly in the child(Male) class
+	}
+};
+
+int main() {
+
+	  Male m1;
+	  // cout <<"Height is: "<< m1.height << endl;  // This will show a compilation error because height is declared protected there in the child class
+
+	  // cout << m1.age << endl;   // this will show compilation error
+    Human h1;
+	  cout << "Age is: " << h1.getAge() << endl;  // Age is: 32759
+	  cout << "Age is: " << m1.getAge() << endl;  // Age is: 0  --> this line shows error if the child(Male) class inherits the private/protected data member of the super(Human) class privately/protectedly
+    
+    m1.print(30);  // this will show compilation error
+}
