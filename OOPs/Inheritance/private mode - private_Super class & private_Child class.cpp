c1da5b1282c85b9dfb19d40264b18a0d2ad7b50b
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-03-22 10:43:10 +0600

    Update private mode - private_Super class & private_Child class.cpp

--- OOPs/Inheritance/private mode - private_Super class & private_Child class.cpp
+++ OOPs/Inheritance/private mode - private_Super class & private_Child class.cpp
@@ -1,3 +1,13 @@
+// Inheritance - memebr of the Super class is in private mode
+
+/* When the member of the super class is in private mode and the child class inherits it from protectedly, private members remain private within the child(Male) class 
+   and they will only be accessible wihtin the super class and the child class but cannot not be accessible outside the super and child class though using member function
+*/
+
+// In C++, private members of a parent class cannot be directly accessed in a child class and outside the child class also, regardless of the type of inheritance used (public, protected, or private).
+// But the inherited member remains accessible but not directly within the child class itself and the super class like in the print1() function as follows
+
+
 #include <iostream>
 using namespace std;
 
