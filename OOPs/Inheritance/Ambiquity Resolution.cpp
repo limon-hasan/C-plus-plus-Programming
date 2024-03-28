a106205ebb4ba055010061b8a872e44b1797cf5d
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-03-28 14:38:05 +0600

    Create Ambiquity Resolution.cpp

---
+++ OOPs/Inheritance/Ambiquity Resolution.cpp
@@ -0,0 +1,49 @@
+#include <iostream>  
+using nampespace std;
+
+class A  
+{  
+public:  
+    void display()  
+    {  
+        cout << "Class A" << std::endl;  
+    }  
+};  
+
+class B  
+{  
+public:  
+    void display()  
+    {  
+        cout << "Class B" << std::endl;  
+    }  
+};  
+
+class C : public A, public B  
+{  
+public:
+    void view()  
+    {  
+        A::display(); // Call display() from class A
+        B::display(); // Call display() from class B
+    }  
+
+    // Override display() in class C
+    void display()
+    {
+        cout << "Class C" << std::endl;
+    }
+};  
+
+int main()  
+{  
+    C c;  
+
+    // Call display() from class C
+    c.display();
+
+    // Call display() from class A and B using view() method
+    c.view();
+
+    return 0;  
+}
