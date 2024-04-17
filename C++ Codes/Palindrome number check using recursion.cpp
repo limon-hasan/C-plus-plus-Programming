4a3fbce9fa4440107b6383ea9ef2255058cfd351
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-04-17 12:54:17 +0600

    Create Palindrome number check using recursion.cpp

---
+++ C++ Codes/Palindrome number check using recursion.cpp
@@ -0,0 +1,25 @@
+// Recursive C++ program to check if the
+// number is palindrome or not --> using recursion
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int rev(int originNum, int reversedNum) {
+	if(originNum == 0) {
+		return reversedNum;
+	}
+
+	reversedNum = (reversedNum * 10) + (originNum % 10);
+	return rev(originNum / 10, reversedNum); 
+}
+
+int main() {
+	int n= 121;
+	int temp = rev(n, 0);
+
+	if(temp == n) {
+		cout << "Yes" << endl;
+	}
+		else
+			cout <<"No" << endl;
+}
