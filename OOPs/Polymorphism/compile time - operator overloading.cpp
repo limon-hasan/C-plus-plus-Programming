33003b36b0797f701c8959cd1db11f1f5b152bd4
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-05-18 13:27:27 +0600

    Create compile time - operator overloading.cpp

---
+++ OOPs/Polymorphism/compile time - operator overloading.cpp
@@ -0,0 +1,48 @@
+// compile time polymorphism
+// operator overloading
+#include<iostream>
+using namespace std;
+
+class B {
+public:
+	int a;
+	int b;
+	int c;
+
+	// (+) overloading
+	void operator+ (B &obj) {
+		int value1 = this->b;
+		int value2 = obj.c;
+		cout <<"Output: " << value1 - value2 << endl;
+
+		cout <<"HEllo Limon " << endl;
+	}
+
+	// bracket() overloading
+	void operator() () {
+		cout <<"I am bracket " << this->a << endl;
+	}
+
+	void operator- (B &obj) {
+		int value1 = this->a;
+		int value2 = obj.b;
+		cout <<"Output: " << value1 * value2 << endl;
+	}
+};
+
+int main() {
+
+	B obj1, obj2, obj3;
+	obj1.a = 10;
+	obj2.b = 3;
+	obj3.c = 1;
+
+	// (+) overlaoding
+	obj2 + obj3;
+	obj1 - obj2;
+
+	// bracket overloading
+	obj1();
+
+	return 0;
+}
