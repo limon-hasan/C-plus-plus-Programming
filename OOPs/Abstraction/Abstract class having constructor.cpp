207204fa41401e1b596b032e0be771d6bcbff65e
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-05-19 04:07:54 +0600

    Update Abstract class having constructor.cpp

--- OOPs/Abstraction/Abstract class having constructor.cpp
+++ OOPs/Abstraction/Abstract class having constructor.cpp
@@ -1,3 +1,5 @@
+// Example - 1
+
 // C++ program to demonstrate that
 // an abstract class can have constructors.
 
@@ -64,3 +66,68 @@ int main(void)
 
 	return 0;
 }
+
+
+
+// ----------------------------------------------
+
+
+
+// Example - 2
+#include <iostream>
+using namespace std;
+
+class Example {
+private:
+    int x;
+    int y;
+    int z;
+
+public:
+    // Constructor with two parameters
+    Example(int a, int b) : x(a), y(b), z(78) {
+        cout << "Base constructor with two parameters called." << endl;
+    }
+
+    // Constructor with one parameter
+    Example(int a) : x(a), y(11), z(12) {
+        cout << "Base constructor with one parameter called." << endl;
+    }
+
+    void print() {
+        cout << "x = " << x << ", y = " << y << ", z = " << z << endl;
+    }
+};
+
+class Derived : public Example {
+private:
+    int w;
+
+public:
+    // Derived class constructor
+    Derived(int a, int b, int c) : Example(a, b), w(c) {
+        cout << "Derived constructor with three parameters called." << endl;
+    }
+
+    Derived(int a, int c) : Example(a), w(c) {
+        cout << "Derived constructor with two parameters called." << endl;
+    }
+
+    void print() {
+        // Call the base class print function
+        Example::print();
+        cout << "w = " << w << endl;
+    }
+};
+
+int main() {
+    // Creating objects of the Derived class
+    Derived obj1(10, 20, 30);
+    Derived obj2(60, 40);
+
+    obj1.print();  // Output: Base and Derived class members
+    obj2.print();  // Output: Base and Derived class members
+
+    return 0;
+}
+
