b56e5bb6d9d7754d7549da39e8f1365494524b81
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-05-18 23:55:04 +0600

    Update complete example.cpp

--- OOPs/Abstraction/complete example.cpp
+++ OOPs/Abstraction/complete example.cpp
@@ -39,3 +39,58 @@ int main(void)
 
 	return 0;
 }
+
+
+
+//-----------------------------------------------------------------
+
+
+// Another example:
+
+// C++ program that demonstrate that we can have pointers and references of abstract class type.
+
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+int value = 9;
+	// pure virtual function
+	virtual void show() = 0;
+};
+
+class Derived : public Base {
+public:
+string name = "limon";
+
+	// implementation of the pure virtual function
+	void show() 
+	{ 
+		cout << "In Derived \n"; 
+	}
+};
+
+int main(void)
+{
+	// creating a pointer of type
+	// Base pointing to an object
+	// of type Derived
+
+	Base* bp = new Derived();
+
+	// calling the show() function using the pointer
+	bp->show();
+
+	Base* b;
+	Derived d;
+	b = &d;
+
+	b->show();
+	d.show();
+	cout << b->value<< endl;
+	cout << d.value<< endl;
+	// cout << b->name << endl;  // this line will show error
+	cout << d.name << endl;
+
+	return 0;
+}
