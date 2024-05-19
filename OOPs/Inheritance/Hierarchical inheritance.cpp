edff3f708090e98757dd5c774e720054dffd42ea
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-05-19 06:04:00 +0600

    Update Hierarchical inheritance.cpp

--- OOPs/Inheritance/Hierarchical inheritance.cpp
+++ OOPs/Inheritance/Hierarchical inheritance.cpp
@@ -1,3 +1,6 @@
+// Hierarchical inheritance --> one class serves as parent class for more than one class
+
+// Example: 1
 #include<iostream>
 using namespace std;
 //Hierarchical Inheritance
@@ -38,4 +41,79 @@ int main() {
     object3.func3();
 
     return 0;
+}
+
+
+
+// --------------------------------------
+
+
+
+// Example - 2
+#include<iostream>
+using namespace std;
+
+class Shape{
+public:
+	int sides;
+	string name;
+
+	public:
+	Shape(string name, int sides) {
+		this->name = name;
+		this->sides = sides;
+	}
+};
+class triangle: public Shape{
+	int height;
+	int base;
+public:
+	triangle(string name, int sides, int base, int height) : Shape(name, sides) {
+		this->base  =base;
+		this->height = height;
+	}
+
+	void area() {
+		cout <<"area of triangle: " << 0.5 * base * height << endl;
+	}
+	void details() {
+		cout << "Sides: " << sides << endl;
+		cout <<"name: " << name << endl;
+		cout << "base: " << base << endl;
+		cout << "height: " << height << endl;
+		area();
+	}
+};
+
+class square: public Shape{
+	private:
+		int height;
+public:
+
+	square(string name, int sides, int height) : Shape(name, sides) {
+		this->height = height;
+	}
+
+	void area() {
+		cout <<"area of square: " << height * height << endl;
+	}
+	void details() {
+		cout << "Sides: " << sides << endl;
+		cout <<"name: " << name << endl;
+		cout << "height: " << height << endl;
+		area();
+	}
+};
+
+int main() 
+{ 
+    // Creating objects 
+    triangle t("triangle", 3, 2, 3); 
+    square s("square", 4, 2); 
+  
+    t.details();
+    cout << endl;
+
+    s.details();
+    cout << endl;
 }
