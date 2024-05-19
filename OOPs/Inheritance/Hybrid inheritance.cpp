adf6634462ff8bf9035d3c4bff26e64242b678b0
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-05-19 07:55:14 +0600

    Update Hybrid inheritance.cpp

--- OOPs/Inheritance/Hybrid inheritance.cpp
+++ OOPs/Inheritance/Hybrid inheritance.cpp
@@ -118,3 +118,82 @@ int main() {
 	joint.withdraw(90);
 	joint.display();
 }
+
+
+
+// ---------------------------------------------------------------------------
+
+
+// EXample: 
+// C++ program to illustrate the hybrid inheritance 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// Base class 
+class Person{
+public:
+	string name;
+
+	Person(string name) : name(name) {
+	}
+
+	void display() {
+		cout << "Name: " << name << endl;
+	}
+};
+
+// Derived class 1: Employee (Single Inheritance) 
+class Employee : public Person {
+protected:
+	int employeeId;
+
+public:
+	Employee(string name, int empId) : Person(name), employeeId(empId) {
+	}
+
+	void displayEmployee() {
+		display();
+		cout << "Employee ID: " << employeeId << endl;
+		cout << "Method inside Derived Class Employee" << endl; 
+	}
+
+};
+
+// Derived class 2: Student (Single Inheritance) 
+class Student : public Person {
+protected:
+	int studentId;
+
+public:
+	Student(string name, int stuId) : Person(name), studentId(stuId) {
+	}
+
+	void displayStudent() {
+		display();
+		cout << "Student ID: " << studentId << endl;
+		cout << "Method inside Derived Class Student" << endl;
+	}
+};
+
+// Derived class 3: StudentIntern (Multiple Inheritance) 
+class StudentIntern : public Student, public Employee {
+
+public:
+	StudentIntern(string name, int stuId, int empId) : Student(name, stuId), Employee(name, empId) {
+	}
+
+    void displayStudentIntern() {
+    	cout << "Methods inside Derived Class StudentIntern : "<< endl; 
+		displayStudent();
+		displayEmployee();
+	}
+};
+
+
+int main() 
+{ 
+	StudentIntern SI("Riya", 67537, 2215); 
+	SI.displayStudentIntern(); 
+
+	return 0; 
+}
