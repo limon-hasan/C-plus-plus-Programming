84efbd86da60e2654dde675afe52d6dcd3e8c424
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-05-21 13:34:06 +0600

    Update palindrome check of string without recursion.cpp

--- C++ Codes/palindrome check of string without recursion.cpp
+++ C++ Codes/palindrome check of string without recursion.cpp
@@ -24,3 +24,51 @@ public:
     return true;
     }
 };
+
+
+
+// ------------------------------
+
+
+// Full code: 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+      
+    int left = 0;
+    int right = s.size() - 1;
+    while(left<= right) {
+        if(!isalnum(s[left])) {
+            left++;
+        }
+        else if(!isalnum(s[right])) {
+            right--;
+        }
+        else if(tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+        else {
+            left++;
+            right--;
+        }
+    }
+    return true;
+    }
+};
+
+int main() {
+
+	string s;
+	getline(cin, s);
+
+	Solution s1;
+	bool ans = s1.isPalindrome(s);
+
+	if(ans == true) 
+		cout <<"Palindrome" << endl;
+	else 
+	cout <<"Not Palindrome" << endl;
+}
