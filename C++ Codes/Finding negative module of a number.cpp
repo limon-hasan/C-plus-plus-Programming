deb48e0ac1f3a9e8f208a2e196db76b1a8055c6e
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-07-07 17:45:31 +0600

    Create Finding negative module of a number.cpp

---
+++ C++ Codes/Finding negative module of a number.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int a = -23;
+    int b = 5;
+    int ans = a % b + b;  // 2   // normally remainder jeta ashbe tar shathe jeta diye module korsi oita sum kore dibo
+    cout << ans << endl;
+}     
