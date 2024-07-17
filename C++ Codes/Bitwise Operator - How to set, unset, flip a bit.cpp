bc25d256058dfd7f0e42da3f53e9e468cb6b3738
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-07-17 20:25:57 +0600

    Create Bitwise Operator - How to set, unset, flip a bit.cpp

---
+++ C++ Codes/Bitwise Operator - How to set, unset, flip a bit.cpp
@@ -0,0 +1,19 @@
+// CodeChef problem - https://www.codechef.com/practice/course/bit-manipulation/BITMF/problems/BITMEDU2
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int ans = n | 1;  // ans = 110 | 001 = 111
+	// cout << ans << endl;  
+	int ans2 = 2;
+	ans2 = ~2;  // ~2 = 1111111101
+	ans = ans & ans2;  // ans = 111 & (~2) = 101
+    // cout << ans << endl; 
+	int ans4 = 1 << 2;  // ans4 = 100
+	ans = ans ^ ans4;   // ans = 101 ^ 100 = 001
+	cout << ans << endl;
+}
