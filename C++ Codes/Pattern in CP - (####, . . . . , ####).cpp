62627984e7a3c7020c66f5ec9b1f129ab7009b72
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-08-28 22:04:31 +0600

    Update Pattern in CP - (####, . . . . , ####).cpp

--- C++ Codes/Pattern in CP - (####, . . . . , ####).cpp
+++ C++ Codes/Pattern in CP - (####, . . . . , ####).cpp
@@ -78,3 +78,40 @@ int main()
         }
     }
 }
+
+
+
+//____________________________________________________________________________________
+
+
+
+// OR -->
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    while(1) {
+        int h, w;
+        cin >> h >> w;
+        if(h == 0 && w == 0) break;
+        else {
+            int cnt = 0;
+            for(int i = 1; i <= h; i++) {
+                if(cnt % 2 == 0) {
+                    for(int j = 1; j <= w; j++) {
+                        cout <<"#";
+                    }
+                }
+                else {
+                    for(int k = 1; k<= w; k++) {
+                        cout <<".";
+                    } 
+                }
+                cout << endl;
+                cnt++;
+            }
+        cout << endl;
+    }
+}
+}
