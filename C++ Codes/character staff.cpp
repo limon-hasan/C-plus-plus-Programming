d27cbb7a9e4dd6cf20b613c8a31e84d4894877c7
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-09-18 10:50:33 +0600

    Create character staff.cpp

---
+++ C++ Codes/character staff.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int p = 'e';  cout << p << endl; // 101
+
+    char q = 'e'; cout << q << endl;  // e
+
+    // printing character from its "Ascii" value
+    cout << (char)('a'+1) << endl;  // b
+    cout << (char)('a'+10) << endl << endl; // k
+
+    // printing character in loop
+    int x = 'a';
+    for(int i = 0; i < 26; i++) {
+        cout << (char)(x++) <<" ";   // a,b,c,d,e,f..............
+    } cout << endl;
+
+
+    // finding position of chracter from taking input of character
+    char c;  
+    cin >> c;
+    cout << c - 'a' << endl;  // 5
+    cout << (int)(c - 'a') << endl;  // 5
+
+    int y = 'd';
+    cout << y << endl; // 1000
+    cout << (char)'d' << endl; // d
+    cout << (char)y << endl;  // d
+
+    char ch = 65;
+    cout << ch << endl; // A
+    cout << (int)ch << endl;  // 65
+
+    char ch1 = '6'; cout << ch1 << endl;
+    
+    string s = "1345"; cout << s << endl;  //1345
+}
