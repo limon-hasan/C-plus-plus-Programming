667b1e8da98270822427d6b6cdeb628d06dc1a15
Maksudul Hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2024-10-04 13:31:16 +0600

    Update palindrome reorder.cpp

--- C++ Codes/palindrome reorder.cpp
+++ C++ Codes/palindrome reorder.cpp
@@ -9,7 +9,8 @@ int temp[mx];
 
 int main() {
     cin >> s;
-    int len = 0;               
+    int len = 0;           
+    // finding string length
     while(s[len] != '\0') {
         len++;
     }   
@@ -30,11 +31,13 @@ int main() {
 
     int oddCount = 0;
     for(int i = 0; i < 26; i++) {
+        // finding the number of odd count
         if(temp[i] % 2 == 1) {
             oddCount++;
         }
     }
-    if(oddCount > 1) {
+    if(oddCount > 1)  // if oddcount is multiple or greater than 1 then it would not be possible to reorder palindrome then we have to break 
+    {
         cout <<"NO SOLUTION" << endl;
         return 0;
     }
@@ -42,19 +45,24 @@ int main() {
     int id = 0;
     for(int i = 0; i < 26; i++) {
         while(temp[i] >= 2) {
+            // Now we have to cut 2 from temp[i = 0,1,2....] that means, if((temp[1] = temp[B]) = 5) then first we have to deduct 2 from temp[B] 
+            // then temp[B] will be temp[B]=3 then we have to insert in s[id = 0,1,2....]th and s[len-id-1]th posiiton          
             temp[i] -= 2;  // temp[1] = temp[B] = 2  // temp[B] - 2 = 2-2 = 0
-            s[id] = i + 'A';
-            s[len - 1- id] = i + 'A';
-            id++;
+            s[id] = i + 'A';   // insert s[id = 0] th position
+            s[len - 1- id] = i + 'A';  // and also insert the same element which is inserted in the s[id] because we have to make palindrome
+            id++;  // after that inserting or fill-upping in these position, we have to increase id(posiiton) to more inserting 
         }
     }
 
+    // now if we have more element(odd number) like temp[B] after inserting in the above and which is odd number like temp[B] = 1 then, we have to inserting it in the gap posiiton
     for(int i = 0; i < 26; i++) {
+        // now we have just 0 or 1 in the temp[] string because after inserting we have ony=ly odd count or 0 in the temp[] but we have n headache with 0 so we have to check with 1 just
         if(temp[i] == 1) {
             s[id] = i + 'A';
         }
     }
 
+    // printing final s[]
     for(int i = 0; s[i] != '\0'; i++) {
         cout << s[i];
     }
